8ef4f538efb5c52abe6158c5e870756a1b27e7e4
Rogevehnam <rogevehnam@gmail.com>
2026-05-10 20:05:22 +0300

    .

--- mods/zzzparanoidal/prototypes/mod_compatibility/Bio_Industries.lua
+++ mods/zzzparanoidal/prototypes/mod_compatibility/Bio_Industries.lua
@@ -8,11 +8,9 @@ if not mods["Bio_Industries"] then return end
 local dart_turret = data.raw["ammo-turret"]["bi-dart-turret"]
 if dart_turret then
     dart_turret.attack_parameters.cooldown = 10  -- было 4; теперь ~6 в/сек
-    dart_turret.attack_parameters.range = 12     -- было 15; короче пулемётной турели (18)
 end
 
 local dart_rifle = data.raw.gun["bi-dart-rifle"]
 if dart_rifle then
     dart_rifle.attack_parameters.cooldown = 15   -- было 5; теперь ~4 в/сек
-    dart_rifle.attack_parameters.range = 14      -- было 17
 end
